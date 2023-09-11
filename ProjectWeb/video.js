658faeda34d9f445f01cafff89fa97a7df7bcc82
Ra-is13 <rais13122544@gmail.com>
2023-09-11 21:37:52 +0700

    Initial commit

---
+++ ProjectWeb/video.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll("[data-carousel- button]")
+buttons.forEach(button => {
+  button.addEventlistener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+     .querySelector(" [data-slides]")
+    const activeslide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexof(activeslide) + offset
+    if (newIndex < 8) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+    slides.children[ newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+})
+})
